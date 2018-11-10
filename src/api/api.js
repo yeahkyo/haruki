e5841a06a74279169ca5129cba41b3b6a4d2ef09
Yeah <zyeah61@gmail.com>
2018-11-10 18:42:20 +0800

    Feature: wechat auth/login/logout

--- src/api/api.js
+++ src/api/api.js
@@ -26,6 +26,46 @@ export default {
         }
       })
     })
+  },
+
+  login() {
+    wx.showLoading({title: "正在登录..."})
+    return new Promise((resolve, reject) => {
+      wx.getUserInfo({
+        success: function(res) {
+          let userInfo = res.userInfo
+          wx.login({
+            success: function(res) {
+              wx.hideLoading()
+              if (res.code) {
+                wx.request({
+                  url: 'http://127.0.0.1:3000/login',
+                  method: 'POST',
+                  header: {
+                    'X-NH-Agent': 'haruki'
+                  },
+                  data: {
+                    code: res.code,
+                    name: userInfo.nickName
+                    // gender: userInfo.gender,
+                    // language: userInfo.language,
+                    // city: userInfo.city,
+                    // province: userInfo.province,
+                    // country: userInfo.country,
+                    // avatarUrl: userInfo.avatarUrl
+                  },
+                  success: function(res) {
+                    resolve(res.data)
+                  }
+                })
+              } else {
+                console.log('登录失败')
+              }
+            }
+          })
+        }
+      })
+    })
   }
 
 }
